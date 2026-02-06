ec3d7ad2c5b6d626cf206037f8e258db39817fac
jhonata jansen <codejhonatajansen@gmail.com>
2026-02-06 11:28:33 -0300

    commit inicial do projeto

---
+++ src/main/java/com/devjansen/literaura/LiterauraApplication.java
@@ -0,0 +1,13 @@
+package com.devjansen.literaura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterauraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterauraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjansen/literaura/LiterauraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjansen.literaura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterauraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
